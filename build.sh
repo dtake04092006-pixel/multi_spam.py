dcc3811a5fc39cee10b61e8ef4293b5a6db09395
Shifty <dtake04092006@gmail.com>
2025-11-06 02:06:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# Cài đặt các thư viện hệ thống
-apt-get update
-apt-get install -y tesseract-ocr
+# Thêm sudo vào 2 dòng dưới
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr
 
-# Chạy pip install cho requirements.txt
+# Dòng này giữ nguyên
 pip install -r requirements.txt
